8feece591fbebb8faf09302e7ebdcd366b5c7e85
singtopza <624259027@webmail.npru.ac.th>
2021-10-11 11:01:51 +0700

    Test Two

--- app/Controllers/Restaurant.php
+++ app/Controllers/Restaurant.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
-use App\models\RestaurantModel;
+#use App\models\RestaurantModel;
 
 class Restaurant extends ResourceController
 {
@@ -16,8 +16,20 @@ class Restaurant extends ResourceController
 
     // Get all Restaurants
     public function index(){
-        $model = new RestaurantModel();
-        $data['restaurants'] = $model->orderBy('id', 'DESC')->findAll();
+
+#        $model = new RestaurantModel();
+#        $data['restaurants'] = $model->orderBy('id', 'DESC')->findAll();
+$data['restaurants'] = $this->model->orderBy('id', 'DESC')->findAll();
         return $this->respond($data);
+    }
+
+    //GET Restaurant by Id
+    public function show($id = null){
+        $data = $this->model->getWhere(['id' => $id])->getResult();
+        if($data){
+            return $this->respond($data);
+        } else{
+            return $this->failNotFound('No Restaurant found with id: ' . $id);
+        }
     }
 }
